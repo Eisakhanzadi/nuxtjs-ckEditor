b687b19059e0a87301264f684cfa6fdbdbdb71ad
eisa-khanzadi <esakhanzadi@gmail.com>
2026-05-08 13:42:59 +0330

    feature(nuxtConfig.ts): add title and icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: {enabled: true},
+    app:{
+      head:{
+          title:'ckEditor-5',
+          link:[{
+              rel:'icon',
+              type:'image/jpeg',
+              href:'/icon.jpg'
+          }]
+      }
+    },
     modules: ['@nuxtjs/tailwindcss'],
     css: ["~/assets/styles/main.scss"],
     runtimeConfig: {
